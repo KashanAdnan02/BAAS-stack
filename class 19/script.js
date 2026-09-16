266830f5371fe42ae2c242fab30d902174330610
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-09-16 02:30:54 -0700

    javascript class 2

---
+++ class 19/script.js
@@ -0,0 +1,86 @@
+alert("Javascript Class 2")
+
+// "kashan" // --> String "a"
+// var name = "Abdul Rahman"
+// var age = 45
+
+// var lastname12$_ = 21
+// var alert = "Kashan"
+
+// var Age = 2
+// var age = 9
+
+// var lastName = "Adnan"
+
+// var kas2han
+
+// var isGoogleVerification =
+
+// var bestSupportingActressInADramaOrComedy = "Kashan Adnan"
+
+// Number cannot be in the starting
+// Dollar Sign Underscore or Letter or number can be in the variable name
+// variable cannot contain spaces
+// varaiable name cannot contain javascript keywords
+// javascript is case sensitive
+// naming convention (camelCase)
+
+
+
+
+// var num1 = 7
+// var num2 = 2
+// // + -> Add
+// // - -> Subtract
+// // * -> Multiply
+// // / -> Divide
+// // % -> Modulus (Remainders)
+// alert(num1 % num2)
+// // alert(num2)
+
+
+
+
+
+var num = 8
+// pre decrement
+// alert(--num)
+
+
+// num + 1
+// post increment
+// alert(num++) // 8 num + 1 = 9
+//
+
+// alert(num) //9
+
+// post decrement  num - 1
+// alert(num--)
+
+// alert(num)// 7
+
+
+// // pre increment
+// ++num
+
+// // post decrement
+// num--
+// // pre decrement
+// --num
+
+// var num = 12
+// alert(num++)  // post increment 12
+// alert(++num) // 14
+// alert(num--) // 14 14-1 = 13
+// alert(--num) //
+
+
+
+var num = 9
+
+alert(num++)  // 9
+alert(--num)  // 9
+alert(num++)  // 9
+alert(++num)  // 11
+
+
