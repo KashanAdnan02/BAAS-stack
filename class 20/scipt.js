0685dd32b2e2a83f247cb47bf743056dcba5c482
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-09-17 02:22:08 -0700

    javascript class 3

---
+++ class 20/scipt.js
@@ -0,0 +1,77 @@
+// alert("Not Found!")
+// Error - Success
+
+// variables
+
+// var name = "Kashan"
+
+// alert(name)
+
+// String -- Number
+
+// var num = 8
+
+// alert(num)
+
+// Illegal
+
+// var 2num = 213
+// var $_askfhaskhf1212 = ""
+// var first name = "Kashan"
+// var firstName = "Kashan"
+
+
+// Familiar Operator
+// +
+// -
+// *
+// /
+// %
+
+// var num1 = 2 % 3
+// alert(num1)
+
+// Unfamilar Operator
+
+// var num = 10
+
+// alert(--num) // 9
+// post increment
+// post decrement
+// pre increment
+// pre decrement
+
+// var num = 1 * 3 + (8 - 9) // BODMAS
+// alert(num)
+
+
+// "kshsa" // -> String
+
+
+// concatenate
+// var userName = "kashan"
+
+// var message = "Thanks " + userName + " for joining!"
+// alert(message)
+
+
+// +  concatenate
+// +  addition
+
+// alert("2" + "2") // 22
+
+// "2" + "2"
+
+// 22
+
+// 4
+
+
+// alert(2 + "2")
+
+
+
+var name = prompt("Enter your name?")
+
+
+alert(name)
